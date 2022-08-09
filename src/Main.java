d1dbc31eb0a43243ca594507591f4c006cd419aa
eaemafe <maria.kbst_17@hotmail.com>
2022-08-09 20:28:30 -0300

    final projeto

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -20,8 +19,44 @@ public class Main {
         mentoria.setDescricao("descrição mentoria de java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMafe = new Dev();
+        devMafe.setNome("Maria Fernanda");
+        devMafe.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Mafe" + devMafe.getConteudosInscritos());
+
+        devMafe.progredir();
+        devMafe.progredir();
+        System.out.println("-");
+
+        System.out.println("Conteúdos Inscritos Mafe" + devMafe.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Mafe" + devMafe.getConteudosConcluidos());
+        System.out.println("XP:" + devMafe.calcularTotalXp());
+
+        System.out.println("------");
+
+        Dev devHeloisa = new Dev();
+        devHeloisa.setNome("Heloisa");
+        devHeloisa.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Heloisa" + devHeloisa.getConteudosInscritos());
+
+        devHeloisa.progredir();
+        devHeloisa.progredir();
+        devHeloisa.progredir();
+        System.out.println("-");
+
+        System.out.println("Conteúdos Inscritos Heloisa" + devHeloisa.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Heloisa" + devHeloisa.getConteudosConcluidos());
+        System.out.println("XP:" + devHeloisa.calcularTotalXp());
     }
 }
